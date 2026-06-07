6b195935b0a10a40df0d75931805c920e3e07050
beratavseren <musaberatavseren@gmail.com>
2026-06-07 18:03:25 +0300

    ci/cd deneme8

--- src/main/java/org/example/cdnp2pstreamingsignaling/Service/ClusteringService.java
+++ src/main/java/org/example/cdnp2pstreamingsignaling/Service/ClusteringService.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ClusteringService {
 
-    //DENEME1
+    //DENEME2
 
     private final PeerNodeRegistry peerNodeRegistry;
     private final PeerNodeService peerNodeService;
